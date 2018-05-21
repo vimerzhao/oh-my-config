c472514178e21718a9e1fa6bc5d19c67126efb05
vimerzhao <dutzhaoyu@gmail.com>
2018-05-21 15:14:00 +0800

    添加git配置文件，防止commit信息显示为8进制

---
+++ git-config/git-config.sh
@@ -0,0 +1,7 @@
+# 1. 字符集设置，使中文字符能正确显示
+# By default, git will print non-ASCII file names in quoted octal notation, 
+# i.e. "\nnn\nnn...". This can be disabled with:
+# git config core.quotepath off
+# Or for all repositories using:
+# git config --global core.quotepath off
+git config --global core.quotepath off
